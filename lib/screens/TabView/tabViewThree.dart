9aa72288252179089d60d0ee209744d9e626605f
Rafi4 <Blood.wars12@gmail.com>
2021-06-12 13:36:19 +0200

    repo 28 Fixed sounds in category Music

--- lib/screens/TabView/tabViewThree.dart
+++ lib/screens/TabView/tabViewThree.dart
@@ -1,6 +1,6 @@
 import 'dart:developer';
-
 import 'package:good_dream/fun/arrays_3-4.dart';
+import 'package:good_dream/fun/toast.dart';
 import 'package:good_dream/models/DataProvider.dart';
 import 'package:good_dream/fun/foregroundService.dart';
 import 'package:flutter/cupertino.dart';
@@ -23,6 +23,7 @@ class TabViewThree extends StatefulWidget {
   @override
   _State createState() => _State(analytics);
 }
+
 class _State extends State<TabViewThree> {
   ThemeMode themeMode = ThemeMode.light;
   // Firebase Analytics
@@ -38,51 +39,43 @@ class _State extends State<TabViewThree> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<DataProvider>(builder: (context,
-        cart,
-        child,) {
+    return Consumer<DataProvider>(builder: (
+      context,
+      cart,
+      child,
+    ) {
       return GridView.builder(
         itemCount: arrays3.length,
-        gridDelegate:
-        SliverGridDelegateWithFixedCrossAxisCount(
-            childAspectRatio: 1.0,
-            crossAxisCount: 3),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            childAspectRatio: 1.0, crossAxisCount: 3),
         itemBuilder: (context, index) {
           return Column(
             children: [
               TextButton(
                 /// Stops all sounds
                 onPressed: () async {
-
                   if (cart.count2 == 1 &&
                       arrays3[index].isFav == false) {
-                    for (int i = 0;
-                   i < arrays3.length;
-                    i++) {
+                    for (int i = 0; i < arrays3.length; i++) {
                       arrays3[i].player.stop();
                       cart.remove2(arrays3[i]);
                       cart.remove(arrays3[i]);
                       arrays3[i].isFav = false;
                     }
                   }
-
-           /*     log('Test audio ---------- ${
+                  /*     log('Test audio ---------- ${
                       arrays3[index].player.open(
                           Audio(arrays3[index].sounds!),)
                   }'
                   );
-
                   log('Test audio STOP 222222 ---------- ${
                       arrays3[index].player.stop()
                   }'
                   );*/
 
-                  if (cart.count2 <= 1 &&
-                      arrays3[index].isFav == false) {
-                    arrays3[index].player.open(
-                        Audio(arrays3[index].sounds!),
-                        volume: 0.5,
-                        loopMode: LoopMode.single);
+                  if (cart.count2 <= 1 && arrays3[index].isFav == false && cart.count <= 5) {
+                    arrays3[index].player.open(Audio(arrays3[index].sounds!),
+                        volume: 0.5, loopMode: LoopMode.single);
                     cart.add2(arrays3[index]);
                     cart.add(arrays3[index]);
                     arrays3[index].isFav = true;
@@ -93,16 +86,21 @@ class _State extends State<TabViewThree> {
                     cart.remove(arrays3[index]);
                     arrays3[index].isFav = false;
                   }
+
+                  if(cart.count == 6 && cart.count2 == 0) {
+                    toast();
+                  }
+
+
                   /// foregroundService START or STOP
                   if (cart.count2 == 1) {
                     foregroundService();
-                  } else if (cart.count2 == 0 &&
-                      cart.count == 0) {
+                  } else if (cart.count2 == 0 && cart.count == 0) {
                     foregroundServiceStop();
                   }
                   // Click_events - if isFav is true
-                 if (arrays3[index].isFav!) {
-                   await _analytics.logEvent(
+                  if (arrays3[index].isFav!) {
+                    await _analytics.logEvent(
                       name: arrays3[index].events!,
                     );
                   }
@@ -113,42 +111,38 @@ class _State extends State<TabViewThree> {
                       height: 50,
                       width: 60,
                       //  height: 50.0,
-                      image: AssetImage(
-                          arrays3[index].isFav!
-                              ? arrays3[index].picOn!
-                              : arrays3[index].picOff!),
+                      image: AssetImage(arrays3[index].isFav!
+                          ? arrays3[index].picOn!
+                          : arrays3[index].picOff!),
                     ),
-                    Padding(
-                        padding:
-                        EdgeInsets.only(top: 10)),
-                    arrays3[index].isFav! ? AnimatedOpacity(
-                      duration: Duration(milliseconds: 700),
-                      opacity: arrays3[index].isFav!
-                          ? arrays3[index].opacityOn
-                          : arrays3[index].opacityOff,
-                      child: PlayerBuilder.volume(
-                          player: arrays3[index].player,
-                          builder: (context, volume) {
-                            return Slider(
-                                activeColor: Colors.grey,
-                                value: volume,
-                                min: 0,
-                                max: 1,
-                                divisions: 50,
-                                onChanged: (v) {
-                                  setState(() {
-                                    arrays3[index]
-                                        .player
-                                        .setVolume(v);
-                                  });
-                                });
-                          }),
-                    ) :  Text(
-                      arrays3[index].title!,
-                      style: TextStyle(fontSize: 13,
-                      color: Colors.white),
-                      textAlign: TextAlign.center,
-                    ),
+                    Padding(padding: EdgeInsets.only(top: 10)),
+                    arrays3[index].isFav!
+                        ? AnimatedOpacity(
+                            duration: Duration(milliseconds: 700),
+                            opacity: arrays3[index].isFav!
+                                ? arrays3[index].opacityOn
+                                : arrays3[index].opacityOff,
+                            child: PlayerBuilder.volume(
+                                player: arrays3[index].player,
+                                builder: (context, volume) {
+                                  return Slider(
+                                      activeColor: Colors.grey,
+                                      value: volume,
+                                      min: 0,
+                                      max: 1,
+                                      divisions: 50,
+                                      onChanged: (v) {
+                                        setState(() {
+                                          arrays3[index].player.setVolume(v);
+                                        });
+                                      });
+                                }),
+                          )
+                        : Text(
+                            arrays3[index].title!,
+                            style: TextStyle(fontSize: 13, color: Colors.white),
+                            textAlign: TextAlign.center,
+                          ),
                   ],
                 ),
               ),
@@ -156,7 +150,6 @@ class _State extends State<TabViewThree> {
           );
         },
       );
-
     });
   }
 }
